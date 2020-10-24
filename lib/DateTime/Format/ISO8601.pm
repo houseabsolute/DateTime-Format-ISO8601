0439a6ac4b45725300849d4ab2b829f618735fd8
Dave Rolsky <autarch@urth.org>
2020-10-24 12:08:00 -0500

    Bump version after release

--- lib/DateTime/Format/ISO8601.pm
+++ lib/DateTime/Format/ISO8601.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 use Carp qw( croak );
 use DateTime 1.45;
--- lib/DateTime/Format/ISO8601/Types.pm
+++ lib/DateTime/Format/ISO8601/Types.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 use parent 'Specio::Exporter';
 
